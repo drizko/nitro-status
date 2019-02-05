d7b69fb4ff0fc0a18955e404c2d08aacea74f26f
Danny Rizko <drizko@servicetitan.com>
2019-02-04 16:50:08 -0800

    Initial Commit

---
+++ index.js
@@ -0,0 +1,50 @@
+import { Component } from "preact";
+import firebase from "firebase/app";
+import config from "./config.js";
+import "firebase/database";
+import "./style";
+
+const firebaseConfig = {
+	apiKey: config.apiKey,
+	authDomain: config.authDomain,
+	databaseURL: config.databaseURL,
+	projectId: config.projectId
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default class App extends Component {
+	state = {
+		full: true
+	};
+
+	updateStatus = () => {
+		console.log(!this.state.full)
+		firebase.database().ref('status').set(!this.state.full)
+	};
+
+	componentDidMount() {
+		firebase
+			.database()
+			.ref("status")
+			.on("value", data => {
+				this.setState({ full: data.val() });
+			});
+	}
+
+	render() {
+		return (
+			<div class="container">
+				<div>Nitro Cold Brew Status</div>
+				<div>
+					{this.state.full ? (
+						<img src="../assets/good.png" />
+					) : (
+						<img src="../assets/bad.png" />
+					)}
+				</div>
+				<button onClick={this.updateStatus}>Update Status</button>
+			</div>
+		);
+	}
+}
